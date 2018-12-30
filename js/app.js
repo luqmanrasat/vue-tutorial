2bf4ed718334630f114fe9e89b4d5841a72d1eb8
Luqman Rasat <luqmanrasat@Luqmans-MacBook-Air.local>
2018-12-31 05:38:06 +0800

    added lesson 14

--- js/app.js
+++ js/app.js
@@ -1,5 +1,12 @@
-new Vue({
-  el: '#vue-app',
+var one = new Vue({
+  el: '#vue-app-one',
+  data: {},
+  methods: {},
+  computed: {}
+});
+
+var two = new Vue({
+  el: '#vue-app-two',
   data: {},
   methods: {},
   computed: {}
