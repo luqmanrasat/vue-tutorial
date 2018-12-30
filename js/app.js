fc4dc2a5c0a75fd2beee5c7711291fbc4114c036
Luqman Rasat <luqmanrasat@Luqmans-MacBook-Air.local>
2018-12-31 06:20:08 +0800

    finish lesson 15

--- js/app.js
+++ js/app.js
@@ -1,5 +1,26 @@
-new Vue({
-  el: '#vue-app',
+Vue.component('greeting', {
+  template: '<p>I am a reusable component. Name: {{ name }} <button @click="changeName">Change name</button></p>',
+  data: function() {
+    return {
+      name: 'Luffy'
+    };
+  },
+  methods: {
+    changeName: function() {
+      this.name = 'Zoro';
+    }
+  }
+});
+
+var one = new Vue({
+  el: '#vue-app-one',
+  data: {},
+  methods: {},
+  computed: {}
+});
+
+var two = new Vue({
+  el: '#vue-app-two',
   data: {},
   methods: {},
   computed: {}
