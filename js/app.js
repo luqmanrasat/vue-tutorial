e306ed82afb45f402556ad78bd237d62231a30fe
Luqman Rasat <luqmanrasat@Luqmans-MacBook-Air.local>
2018-12-31 05:34:28 +0800

    lesson template

--- js/app.js
+++ js/app.js
@@ -1,21 +1,6 @@
 new Vue({
   el: '#vue-app',
-  data: {
-    age: 27,
-    x: 0,
-    y: 0
-  },
-  methods: {
-    add: function(inc) {
-      this.age += inc;
-    },
-    subtract: function(dec) {
-      this.age-= dec;
-    },
-    updateXY: function(event) {
-      console.log(event);
-      this.x = event.offsetX;
-      this.y = event.offsetY;
-    }
-  }
+  data: {},
+  methods: {},
+  computed: {}
 });
