744593a6e2e04ecb398efcedccda5a271e89aa9a
Luqman Rasat <luqmanrasat@Luqmans-MacBook-Air.local>
2018-12-31 16:08:39 +0800

    add animation & transition tutorial

--- js/app.js
+++ js/app.js
@@ -1,6 +1,34 @@
-new Vue({
+var app = new Vue({
   el: '#vue-app',
-  data: {},
-  methods: {},
-  computed: {}
+  data: {
+    show0: true,
+    show1: true,
+    show2: true,
+    show3: true,
+    show4: false,
+    view: 'component1',
+    styletrans: {
+      fontSize:'30px',
+      color:'red'
+    }
+  },
+  methods: {
+    beforeEnter: function(el) {
+      el.style.opacity = 0;
+    },
+    enter: function(el, done) {
+      Velocity(el, { opacity: 1, fontSize: '25px' }, { duration: 1000 });
+      Velocity(el, { fontSize: '10px' }, { complete: done });
+    },
+    leave: function(el, done) {
+      Velocity(el, { translateX: '15px', rotateZ: '50deg' }, { duration: 1500 });
+      Velocity(el, { rotateZ: '100deg' }, { loop: 2 });
+      Velocity(el, { rotateZ: '45deg', translateY: '30px', translateX: '30px', opacity: 0 }, { complete: done });
+    }
+  },
+  components: {
+    'component1': {
+      template: '<div><span style="font-size: 25; color: red;">Animation on Components</span></div>'
+    }
+  }
 });
