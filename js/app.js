507106e204ca22fe2d20a7fe518066e6de9ebabb
Luqman Rasat <luqmanrasat@Luqmans-MacBook-Air.local>
2018-12-31 12:14:31 +0800

    added conditionals and iterations tutorial

--- js/app.js
+++ js/app.js
@@ -1,6 +1,22 @@
-new Vue({
+var app = new Vue({
   el: '#vue-app',
-  data: {},
-  methods: {},
+  data: {
+    show: false,
+    items: [],
+    styleiter: {
+      width: "30%",
+      padding: "12px 20px",
+      margin: "8px 0",
+      boxSizing: "border-box"
+    }
+  },
+  methods: {
+    showpara: function() {
+      this.show = !this.show;
+    },
+    showinputvalue: function(event) {
+      this.items.push(event.target.value);
+    }
+  },
   computed: {}
 });
