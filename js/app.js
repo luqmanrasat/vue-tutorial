1653d3142a9778169896befa0fa96b959f73a28d
Luqman Rasat <luqmanrasat@Luqmans-MacBook-Air.local>
2018-12-31 17:27:42 +0800

    add routing tutorial

--- js/app.js
+++ js/app.js
@@ -1,6 +1,14 @@
-new Vue({
+const Route1 = { template: '<div class="box box--rounded box--cyan">This is router 1</div>' };
+const Route2 = { template: '<div class="box box--rounded box--green">This is router 2</div>' };
+const routes = [
+  { path: '/route1', component: Route1 },
+  { path: '/route2', component: Route2}
+];
+const router = new VueRouter({
+  routes // short for 'routes: routes'
+});
+
+var vm = new Vue({
   el: '#vue-app',
-  data: {},
-  methods: {},
-  computed: {}
+  router
 });
