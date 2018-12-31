db8aa084b82de530569ec29430a756c204802687
Luqman Rasat <luqmanrasat@Luqmans-MacBook-Air.local>
2018-12-31 16:50:04 +0800

    add directives tutorial

--- js/app.js
+++ js/app.js
@@ -1,6 +1,29 @@
-new Vue({
+Vue.directive("changestyle0", {
+  bind(e1, binding, vnode) {
+    console.log(e1);
+    e1.style.color="red";
+    e1.style.fontSize="30px";
+  }
+});
+
+Vue.directive("changestyle1", {
+  bind(e1, binding, vnode) {
+    console.log(e1);
+    console.log(binding.value.color);
+    console.log(vnode);
+    e1.style.color = binding.value.color;
+    e1.style.fontSize = "30px";
+  }
+});
+
+var app = new Vue({
   el: '#vue-app',
-  data: {},
-  methods: {},
-  computed: {}
+  data: {
+    name: ''
+  },
+  filters: {
+    countletters: function(value) {
+      return value.length;
+    }
+  }
 });
